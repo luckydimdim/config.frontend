f306f6197043247a5534ad5461174a55657dc5da
Dmitriy <Dmitriy.Litovchenko@yamalspg.ru>
2017-03-07 13:55:01 +0300

    int =< String

--- lib/src/config_helper.dart
+++ lib/src/config_helper.dart
@@ -17,21 +17,21 @@ class ConfigHelper {
   ConfigHelper(ConfigService service) {
     _service = service;
 
-    _service.Get('backend_scheme')
+    _service.Get<String>('backend_scheme')
       .then((value) {
         backendScheme = value;
-        _service.Get('backend_base_url').then((value) {
+        _service.Get<String>('backend_base_url').then((value) {
           backendBaseUrl = value;
-          _service.Get('backend_port').then((value) {
+          _service.Get<String>('backend_port').then((value) {
             backendPort = value;
             backendUrl = '$backendScheme://$backendBaseUrl:$backendPort';
           }).then((value) {
 
-            _service.Get('backend_contracts').then((value) {
+            _service.Get<String>('backend_contracts').then((value) {
               contractsUrl = '$backendUrl/$value';
             });
 
-            _service.Get('backend_logs').then((value) {
+            _service.Get<String>('backend_logs').then((value) {
               logsUrl = '$backendUrl/$value';
             });
 
